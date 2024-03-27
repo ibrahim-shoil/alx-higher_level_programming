c0dbf58be9bf03c9e54d405d6fc085639a6e340f
ibrahim-shoil <ibrahimshoil50@gmail.com>
2024-03-27 03:33:42 +0200

    Auto commit: 2024-03-27 03:33:42

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def to_upper_case(s):
+    result = ''
+    for char in s:
+        if 'a' <= char <= 'z':
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    return result

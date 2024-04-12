5187225756afd7932b026dfe97c07add576c1ad7
ibrahim-shoil <ibrahimshoil50@gmail.com>
2024-04-12 02:54:31 +0200

    Auto commit: 2024-04-12 02:54:31

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    argsLst = sys.argv
-    lenArgs = len(argsLst)
+    args_list = sys.argv
+    len_args = len(args_list)
     idx = 1
-    if lenArgs >= 2:
-        if lenArgs == 2:
-            print('{} argument:'.format(lenArgs - 1))
+    if len_args >= 2:
+        if len_args == 2:
+            print('{} argument:'.format(len_args - 1))
         else:
-            print('{} arguments:'.format(lenArgs - 1))
-        for args in range(lenArgs - 1):
-            print('{}: {}'.format(idx, argsLst[idx]))
+            print('{} arguments:'.format(len_args - 1))
+        for args in range(len_args - 1):
+            print('{}: {}'.format(idx, args_list[idx]))
             idx += 1
     else:
-        print('0 arguments')
+        print('0 arguments.')

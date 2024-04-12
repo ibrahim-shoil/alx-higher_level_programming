6011f22175b06ee4a483032a7ce9665dc3a31ee1
ibrahim-shoil <ibrahimshoil50@gmail.com>
2024-04-12 02:47:28 +0200

    Auto commit: 2024-04-12 02:47:28

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
-
+if __name__ == "__main__":
+    import sys
+    argsLst = sys.argv
+    lenArgs = len(argLst)
+    idx = 1
+    if lenArgs >= 2:
+        if lenArgs == 2:
+            print('{} argument:'.format(lenArgs - 1))
+        else:
+            print('{} arguments:'.format(lenArgs - 1))
+        for args in range(lenArgs - 1):
+            print('{}: {}'.format(idx, argLst[idx]))
+        idx += 1
+    else:
+        print('0 arguments')
